c13983d391e87ecb05d75560ea7c521d800b700a
anhquoctran <aquoc.hue@outlook.com>
2026-08-22 13:15:18 +0700

    feat: add PWA manifest icons and implement custom native splash screen in index.html

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -53,10 +53,26 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
             src: '/favicon.svg',
             sizes: '192x192 512x512',
             type: 'image/svg+xml',
-            purpose: 'any maskable',
+            purpose: 'any',
           },
         ],
       },
